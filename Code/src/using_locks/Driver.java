c3e8431d9396bd5505fe2dd296477290c3e46943
fweezee <jordangana23@gmail.com>
2017-07-12 00:09:39 +0800

    I added ung sa Stations

--- Code/src/using_locks/Driver.java
+++ Code/src/using_locks/Driver.java
@@ -1,10 +1,20 @@
 package using_locks;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Driver {
-	
+
+    private static Lock station = new ReentrantLock();
+    private static Condition arrival = station.newCondition();
 		
 	public static void main(String[] args){
 		
-		CalTrain calTrain = new CalTrain();	
+		CalTrain calTrain = new CalTrain();
+
+
+
+
 	}
 }
