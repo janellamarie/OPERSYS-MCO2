4ad0d596dbe3d9267c10f80ae112c51e0047882d
nella <janella_marie_co@dlsu.edu.ph>
2017-07-23 14:57:40 +0800

    ded
    hello

--- Code/src/cal_train/Passenger.java
+++ Code/src/cal_train/Passenger.java
@@ -1,38 +1,83 @@
 package cal_train;
 
-/**
- * Created by Jords on 7/18/2017.
- */
-public class Passenger extends Thread{
-    private Station curr_station;
-    private Station destination;
+public class Passenger implements Runnable {
 
-    public Passenger(Station curr_station, Station destination){
-        this.curr_station = curr_station;
-        this.destination = destination;
-    }
+	private Station destination;
+	private Station current;
+	
+	@Override
+	public void run() {
+		/* run passenger thread */
+		if(CalTrain.solType){
+			current.passengers_waiting(this);
+		} else {
+			System.out.println("RUNNING PASSENGER THREAD");
+			station_wait_for_train(current);
+		}
+	}
+		
+	public Passenger(Station current, Station destination){
+		this.current = current;
+		this.destination = destination;
+		current.addPassenger(this);
+	}
+	
+	public void station_wait_for_train(Station station){
+		/* called when passenger robot arrives in the station */
+		System.out.println("PASSENGER: wait_for_train in Station " + current.getStation_number());
+		
+		System.out.println("TOTAL PASSENGERS WAITING IN STATION: " + current.getPassengers().size());
+		
+		try{		
+			while(!CalTrain.mutex.tryAcquire()){
+				/* while another thread is in the cs */
+			}
+			
+			System.out.println("--> Passenger acquired mutex ");
 
-    public Station getCurr_station() {
-        return curr_station;
-    }
+			if(station.getStationSemaphore().availablePermits() == 0){
+				/* CRITICAL SECTION -> passenger is boarding */
+				station_on_board(station);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			System.out.println("--> Passenger released mutex");
+			CalTrain.mutex.release();
+		}		
+	}
+	
+	public void station_on_board(Station station){
+		/* called when passenger is seated */
+		
+		Train currTrain = station.getCurrentTrain();
+	
+		currTrain.addPassenger(this);
+		
+		System.out.println("PASSENGER HAS BOARDED TRAIN");
+	}
 
-    public void setCurr_station(Station curr_station) {
-        this.curr_station = curr_station;
-    }
+	/* SETTERS AND GETTERS */
+	
+	public Station getCurrent() {
+		return current;
+	}
 
-    public Station getDestination() {
-        return destination;
-    }
+	public void setCurrent(Station current) {
+		this.current = current;
+	}
 
-    public void setDestination(Station destination) {
-        this.destination = destination;
-    }
+	public Station getDestination() {
+		return destination;
+	}
 
-    public void run(){
-        //I removes this because d naman ATA kailangan
-        //curr_station.addPassenger(this);
-        // curr_station.passengers_waiting(this);
-        //curr_station.calTrain.station_wait_for_train(curr_station);
-        curr_station.passengers_waiting(this);
-    }
+	public void setDestination(Station destination) {
+		this.destination = destination;
+	}
+
+	@Override
+	public String toString() {
+		return "Passenger [destination=" + destination.getStation_number() + ", current=" + current.getStation_number() + "]";
+	}
 }
